ebe592cb53894d34143d2dbbab92914d8a4d1efb
Bhushan patil <bhushanvasantpatil@gmail.com>
2021-09-26 14:53:41 -0400

    my secondcommit reacteweb

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Header from './Header';
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
+import SearchPage from './SearchPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+       <Switch>
+       <Route path="/search/:searchTerm">
+       <Header/>
+       <div className="app__page">
+      <Sidebar/>
+     <SearchPage/>
+    </div>
+         </Route>
+
+         <Route path="/">
+         <Header/>
+     <div className="app__page">
+      <Sidebar/>
+     <RecommendedVideos/>
+    </div>
+
+         </Route>
+
+       </Switch>
+      </Router>
+   
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RecommendedVideos.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './RecommendedVideos.css'
+import VideoCard from './VideoCard'
+
+function RecommendedVideos() {
+    return (
+        <div className='recommendedVideos'>
+            <h2>Recommended </h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard
+                title="COPYWRITING with AI?"
+                views="2.3m Views"
+                timestap="3 day ago"
+                channelImage="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                channel=" Jay kooper"
+                image="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                />
+               <VideoCard
+                title="COPYWRITING with AI?"
+                views="2.3m Views"
+                timestap="3 day ago"
+                channelImage="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                channel=" Jay kooper"
+                image="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                />
+                <VideoCard
+                title="COPYWRITING with AI?"
+                views="2.3m Views"
+                timestap="3 day ago"
+                channelImage="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                channel=" Jay kooper"
+                image="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                />
+                <VideoCard
+                title="COPYWRITING with AI?"
+                views="2.3m Views"
+                timestap="3 day ago"
+                channelImage="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                channel=" Jay kooper"
+                image="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                />
+                <VideoCard
+                title="COPYWRITING with AI?"
+                views="2.3m Views"
+                timestap="3 day ago"
+                channelImage="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                channel=" Jay kooper"
+                image="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                />
+                <VideoCard
+                title="COPYWRITING with AI?"
+                views="2.3m Views"
+                timestap="3 day ago"
+                channelImage="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                channel=" Jay kooper"
+                image="https://img.youtube.com/vi/-NYCNQvzVvg/sddefault.jpg"
+                />
+              
+
+            </div>
+        </div>
+    )
+}
+
+export default RecommendedVideos
